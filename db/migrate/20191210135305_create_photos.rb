44440863c661c0ea38325e49b1531d7f00c8f503
Aden Mengistie <adensm@icloud.com>
2019-12-12 05:28:02 +0000

    Add Image

--- db/migrate/20191210135305_create_photos.rb
+++ db/migrate/20191210135305_create_photos.rb
@@ -2,6 +2,7 @@ class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
       t.text :caption
+      t.string :picture
       t.integer :place_id
       t.timestamps
     end
